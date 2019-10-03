e696c3753eb7a54e5ce24666b94d3f93daa9560d
Andreas Säuberli <andreas.saeuberli@gmail.com>
2019-10-03 07:37:01 +0200

    initial commit

---
+++ js/states/play.js
@@ -0,0 +1,14 @@
+game.PlayStage = me.Stage.extend({
+
+    onResetEvent: function() {
+        this.player = new me.Sprite(0, 0, {image: "test"});
+        me.game.world.reset();
+        me.game.world.addChild(new me.ColorLayer("background", "#000000"), 0)
+        me.game.world.addChild(this.player);
+    },
+
+    onDestroyEvent: function() {
+
+    }
+
+});
