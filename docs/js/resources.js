c0e55377bc88610cf31a52f4c5f154682ce9aabe
Andreas Säuberli <andreas.saeuberli@gmail.com>
2019-10-06 23:02:01 +0200

    playable version!

---
+++ docs/js/resources.js
@@ -0,0 +1,232 @@
+game.resources = [
+    {
+        "name": "LD_boss_level",
+        "type": "audio",
+        "src": "res/bgm/"
+    },
+    {
+        "name": "LD_minimal",
+        "type": "audio",
+        "src": "res/bgm/"
+    },
+    {
+        "name": "banquier_hair",
+        "type": "image",
+        "src": "res/img/banquier_hair.png"
+    },
+    {
+        "name": "banquier_jacket",
+        "type": "image",
+        "src": "res/img/banquier_jacket.png"
+    },
+    {
+        "name": "banquier_pants",
+        "type": "image",
+        "src": "res/img/banquier_pants.png"
+    },
+    {
+        "name": "banquier_screen",
+        "type": "image",
+        "src": "res/img/banquier_screen.png"
+    },
+    {
+        "name": "barca_hair",
+        "type": "image",
+        "src": "res/img/barca_hair.png"
+    },
+    {
+        "name": "barca_jacket",
+        "type": "image",
+        "src": "res/img/barca_jacket.png"
+    },
+    {
+        "name": "barca_pants",
+        "type": "image",
+        "src": "res/img/barca_pants.png"
+    },
+    {
+        "name": "barca_screen",
+        "type": "image",
+        "src": "res/img/barca_screen.png"
+    },
+    {
+        "name": "cop_hair",
+        "type": "image",
+        "src": "res/img/cop_hair.png"
+    },
+    {
+        "name": "cop_jacket",
+        "type": "image",
+        "src": "res/img/cop_jacket.png"
+    },
+    {
+        "name": "cop_pants",
+        "type": "image",
+        "src": "res/img/cop_pants.png"
+    },
+    {
+        "name": "cowboy_hair",
+        "type": "image",
+        "src": "res/img/cowboy_hair.png"
+    },
+    {
+        "name": "cowboy_jacket",
+        "type": "image",
+        "src": "res/img/cowboy_jacket.png"
+    },
+    {
+        "name": "cowboy_pants",
+        "type": "image",
+        "src": "res/img/cowboy_pants.png"
+    },
+    {
+        "name": "cowboy_screen",
+        "type": "image",
+        "src": "res/img/cowboy_screen.png"
+    },
+    {
+        "name": "elvis_hair",
+        "type": "image",
+        "src": "res/img/elvis_hair.png"
+    },
+    {
+        "name": "elvis_jacket",
+        "type": "image",
+        "src": "res/img/elvis_jacket.png"
+    },
+    {
+        "name": "elvis_pants",
+        "type": "image",
+        "src": "res/img/elvis_pants.png"
+    },
+    {
+        "name": "elvis_screen",
+        "type": "image",
+        "src": "res/img/elvis_screen.png"
+    },
+    {
+        "name": "gameover",
+        "type": "image",
+        "src": "res/img/gameover.png"
+    },
+    {
+        "name": "naked_blush",
+        "type": "image",
+        "src": "res/img/naked_blush.png"
+    },
+    {
+        "name": "naked_RAW",
+        "type": "image",
+        "src": "res/img/naked_RAW.png"
+    },
+    {
+        "name": "shoe",
+        "type": "image",
+        "src": "res/img/shoe.png"
+    },
+    {
+        "name": "sider_sandhour",
+        "type": "image",
+        "src": "res/img/sider_sandhour.png"
+    },
+    {
+        "name": "sider",
+        "type": "image",
+        "src": "res/img/sider.png"
+    },
+    {
+        "name": "skater_hair",
+        "type": "image",
+        "src": "res/img/skater_hair.png"
+    },
+    {
+        "name": "skater_jacket",
+        "type": "image",
+        "src": "res/img/skater_jacket.png"
+    },
+    {
+        "name": "skater_pants",
+        "type": "image",
+        "src": "res/img/skater_pants.png"
+    },
+    {
+        "name": "skater_screen",
+        "type": "image",
+        "src": "res/img/skater_screen.png"
+    },
+    {
+        "name": "soundhour_background",
+        "type": "image",
+        "src": "res/img/soundhour_background.png"
+    },
+    {
+        "name": "spickel_air",
+        "type": "image",
+        "src": "res/img/spickel_air.png"
+    },
+    {
+        "name": "spickel_sand",
+        "type": "image",
+        "src": "res/img/spickel_sand.png"
+    },
+    {
+        "name": "street",
+        "type": "image",
+        "src": "res/img/street.png"
+    },
+    {
+        "name": "success",
+        "type": "image",
+        "src": "res/img/success.png"
+    },
+    {
+        "name": "swimmer_hair",
+        "type": "image",
+        "src": "res/img/swimmer_hair.png"
+    },
+    {
+        "name": "swimmer_jacket",
+        "type": "image",
+        "src": "res/img/swimmer_jacket.png"
+    },
+    {
+        "name": "swimmer_pants",
+        "type": "image",
+        "src": "res/img/swimmer_pants.png"
+    },
+    {
+        "name": "swimmer_screen",
+        "type": "image",
+        "src": "res/img/swimmer_screen.png"
+    },
+    {
+        "name": "titlescreen",
+        "type": "image",
+        "src": "res/img/titlescreen.png"
+    },
+    {
+        "name": "victory",
+        "type": "image",
+        "src": "res/img/victory.png"
+    },
+    {
+        "name": "wizard_hair",
+        "type": "image",
+        "src": "res/img/wizard_hair.png"
+    },
+    {
+        "name": "wizard_jacket",
+        "type": "image",
+        "src": "res/img/wizard_jacket.png"
+    },
+    {
+        "name": "wizard_pants",
+        "type": "image",
+        "src": "res/img/wizard_pants.png"
+    },
+    {
+        "name": "wizard_screen",
+        "type": "image",
+        "src": "res/img/wizard_screen.png"
+    }
+];
